c472b8dfb5e927c670807b9c6eaf39e1c5eac906
Jeryel Blanco <jeryelblanco@gmail.com>
2023-02-22 18:09:05 -0500

    Day2HW

---
+++ index.js
@@ -0,0 +1,69 @@
+console.log("Hello world!")
+//? Does line 1 show up in your console, if it does not
+//? check the HTML and make sure the script is linked correctly
+
+//! Create an object that contains five movies. Each of these
+//! movies will have a title, and an image url
+//* Note: Find images in google and click on "copy image address"
+//* in the drop down when doing a two finger click on the image
+//* paste this into your image url
+
+const movies = [
+    {
+/* movie objects go here */
+
+},
+{
+    /* movie objects go here */
+
+    },
+    {
+        /* movie objects go here */
+
+        }
+        ,
+    {
+        /* movie objects go here */
+
+        }
+]
+
+
+
+//! In the HTML, there is an id associated with a div element
+//TODO: Get the element with the id of "body"
+////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////
+//TODO: Iterate throgh the object using forEach
+////////////////////////////////////////////////////////
+/* fill this in */.forEach(data => {
+//TODO: Create an "h3" and "img" tag that will contain your
+//TODO: movies object's title and image url
+//! REMEMBER, you want to create these HTML tags FOR EACH
+//! movie inside your object 
+////////////////////////////////////////////////////////
+
+
+
+////////////////////////////////////////////////////////
+//TODO: Modify the inner text of the elements created
+//TODO: to match your movies object
+////////////////////////////////////////////////////////
+
+
+
+////////////////////////////////////////////////////////
+//! Bonus
+//TODO: create a "click" event that changes the title
+//TODO: of the movie when you click the image
+////////////////////////////////////////////////////////
+
+
+
+////////////////////////////////////////////////////////
+//* Keep in mind that I purposed kept all of this code 
+//* inside the for each iteration so we won't run into 
+//* scope problems
+})
+
