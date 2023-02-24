97153bb1656244a474f7ca0d03f9ad0b0612d32f
Nehemiah Henry <nehemiahelihenry@gmail.com>
2023-02-24 09:50:38 -0500

    Assignment Completed

--- index.js
+++ index.js
@@ -10,22 +10,25 @@ console.log("Hello world!")
 
 const movies = [
     {
-/* movie objects go here */
-
-},
-{
-    /* movie objects go here */
-
+        title: "Captain America: The Winter Soldier", 
+        image: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRjtj6Pi8ihy3fam85Khujh6a9_XMTxJj1g2xAkKFMYIwcZ8Vm2"
     },
     {
-        /* movie objects go here */
-
-        }
-        ,
+        title: "Kill Bill",
+        image: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRC9sx_lmH9kAq2nwcpn56zT8h12hkvAfGsWgEnP3reJ9hA_w5_"
+    },
     {
-        /* movie objects go here */
-
-        }
+        title: "The Lion King",
+        image: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRpGOeTdpPET8OvEtjBBg03Wze_EZKu61WNaK4mxfoVcPZmZEN6"
+    },
+    {
+        title: "Tinker Tailor Soldier Spy",
+        image: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcR4R1hfBTm41xYmhMGuB8JQYNlJC2apETmx9p6-hlZllO6MsZdV"
+    },
+    {
+        title: "A Silent Voice",
+        image: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQJrlYXvqlm1bAFMMfjhlX970K4B0z2AJv66KKb1Y9gW-4eml2X"
+    },
 ]
 
 
@@ -33,32 +36,36 @@ const movies = [
 //! In the HTML, there is an id associated with a div element
 //TODO: Get the element with the id of "body"
 ////////////////////////////////////////////////////////
-
+const docDiv = document.querySelector("#body")
 ////////////////////////////////////////////////////////
 //TODO: Iterate throgh the object using forEach
 ////////////////////////////////////////////////////////
-/* fill this in */.forEach(data => {
+movies.forEach(data => {
+    
 //TODO: Create an "h3" and "img" tag that will contain your
 //TODO: movies object's title and image url
 //! REMEMBER, you want to create these HTML tags FOR EACH
 //! movie inside your object 
 ////////////////////////////////////////////////////////
+    const movieHeader = document.createElement("h3")
+    const movieImage = document.createElement("img")
 
-
-
+    docDiv.append(movieHeader)
+    docDiv.append(movieImage)
 ////////////////////////////////////////////////////////
 //TODO: Modify the inner text of the elements created
 //TODO: to match your movies object
 ////////////////////////////////////////////////////////
-
-
-
+    movieHeader.innerText = data.title
+    movieImage.src = data.image
 ////////////////////////////////////////////////////////
 //! Bonus
 //TODO: create a "click" event that changes the title
 //TODO: of the movie when you click the image
 ////////////////////////////////////////////////////////
-
+    movieImage.addEventListener("click", ()=> {
+        movieHeader.innerText = "Look, don't touch"
+    })
 
 
 ////////////////////////////////////////////////////////
